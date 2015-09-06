088a0952bcd0bcd6608c7a632be65969641d08e3
Bert Santema <atsantema@hotmail.com>
2015-09-07 01:06:46 +0200

    Groups call includes balance for each group member.

--- Models/Member.php
+++ Models/Member.php
@@ -14,8 +14,6 @@ class Member
 {
     function getGroupsBalance($uid)
     {
-        // TODO: rewrite for all groups that this user is in (and all user balances in these groups)
-
         // get all the groups for this user
         $sql = "SELECT users_groups.group_id AS gid,
                 groups.name, shortname AS role, UNIX_TIMESTAMP(join_date) AS join_date, groups.description,
@@ -29,36 +27,64 @@ class Member
         $groups = $stmt->fetchAll(\PDO::FETCH_ASSOC);
         $groups = $this->rearrangeArrayKey('gid', $groups);
 
-        // Get the total expense for this user in each group
+        // get all the users that are members of these groups from the concatenated field
+        $user_id_list = array();
+        foreach ($groups as $group)
+            $user_id_list = array_merge($user_id_list, explode(',',$group['user_id_list']));
+        $user_id_list = implode(',', array_unique($user_id_list));
+
+        // get group list for use in query
+        $group_id_list = implode(',', array_keys($groups));
+
+        // Get the total expense for all users in each group
+        // Use find in set, see here: http://stackoverflow.com/questions/1586587/pdo-binding-values-for-mysql-in-statement
         $sql = "SELECT userid, group_id, SUM(expenses_summary.per_person) AS expense FROM (
-                  SELECT users_expenses.user_id AS userid, group_id, users_expenses.expense_id AS exid, amount AS total_amount,
+                    SELECT users_expenses.user_id AS userid, group_id, users_expenses.expense_id AS exid,
+                    amount AS total_amount,
                     (SELECT COUNT(*) FROM users_expenses WHERE expense_id = exid) AS member_count,
                     (SELECT total_amount/member_count) AS per_person
-                  FROM users_expenses, expenses WHERE users_expenses.expense_id = expenses.expense_id AND users_expenses.user_id = :uid
-                  ORDER BY userid, group_id ASC, users_expenses.expense_id ASC
-                  ) AS expenses_summary
+                  FROM users_expenses, expenses WHERE users_expenses.expense_id = expenses.expense_id
+                  AND FIND_IN_SET(users_expenses.user_id, :uids) AND FIND_IN_SET(expenses.group_id, :gids)) AS expenses_summary
                 GROUP BY userid, group_id";
         $stmt = Db::getInstance()->prepare($sql);
-        $stmt->execute(array(':uid' => $uid));
+        $stmt->execute(array(':uids' => $user_id_list, 'gids' => $group_id_list));
         $expense_summary = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-        $expense_summary = $this->rearrangeArrayKey('group_id', $expense_summary);
 
-        // Get the total paid by this user in each group
-        $sql = "SELECT user_id, group_id, SUM(amount) AS paid from expenses WHERE user_id = :uid GROUP BY user_id, group_id";
-        $stmt = Db::getInstance()->prepare($sql);
-        $stmt->execute(array(':uid' => $uid));
-        $paid_summary = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-        $paid_summary = $this->rearrangeArrayKey('group_id', $paid_summary);
-
-        // foreach group add expense, paid and balance
-        foreach ($groups as $group_id => $group) {
-            $expense = array_key_exists($group_id, $expense_summary) ? floatval($expense_summary[$group_id]['expense']) : 0;
-            $paid = array_key_exists($group_id, $paid_summary) ? floatval($paid_summary[$group_id]['paid']) : 0;
-            $groups[$group_id]['expense'] = $expense;
-            $groups[$group_id]['paid'] = $paid;
-            $groups[$group_id]['balance'] = $paid-$expense;
+        // add expense costs for each user to groups array, skip groups for which this user is not a member
+        foreach ($expense_summary as $userJoined){
+            if (!array_key_exists($userJoined['group_id'], $groups))
+                continue;
+            if (!array_key_exists('members', $groups[$userJoined['group_id']]))
+                $groups[$userJoined['group_id']]['members'] = array();
+            $groups[$userJoined['group_id']]['members'][$userJoined['userid']] =
+                array('expense' => floatval($userJoined['expense']), 'paid' => 0, 'balance' => -floatval($userJoined['expense']));
         }
 
+        // Get the total paid by this user in each group
+        $sql = "SELECT user_id, group_id, SUM(amount) AS paid FROM expenses WHERE FIND_IN_SET(expenses.user_id, :uids) GROUP BY user_id, group_id";
+        $stmt = Db::getInstance()->prepare($sql);
+        $stmt->execute(array(':uids' => $user_id_list));
+        $paid_summary = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        // add paid balance for each user to groups array, skip groups for which this user is not a member
+        foreach ($paid_summary as $userPaid){
+            $gid = $userPaid['group_id'];
+            $p_uid = $userPaid['user_id'];
+            if (!array_key_exists($gid, $groups))
+                continue;
+            if (!array_key_exists('members', $groups[$gid]))
+                $groups[$gid]['members'] = array();
+            if (!array_key_exists($p_uid , $groups[$gid]['members'])) {
+                $groups[$gid]['members'][$p_uid] =
+                    array('expense' => 0, 'paid' => 0, 'balance' => 0);
+            }
+            $groups[$gid]['members'][$p_uid]['paid'] = floatval($userPaid['paid']);
+            $groups[$gid]['members'][$p_uid]['balance'] = floatval($userPaid['paid']) - $groups[$gid]['members'][$p_uid]['expense'];
+        }
+
+        foreach ($groups as &$group){
+            $group['balance'] = $group['members'][$uid]['balance'];
+        }
         return json_encode(($groups));
     }
 
@@ -71,11 +97,10 @@ class Member
     {
         $newArray = array();
         foreach ($array as $item) {
-            if (array_key_exists($keyname,$item)) {
+            if (array_key_exists($keyname, $item)) {
                 $newArray[$item[$keyname]] = $item;
             }
         }
         return empty($newArray) ? $array : $newArray;
     }
-
 }
